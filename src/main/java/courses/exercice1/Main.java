2946908a4b3427f5683504245602fcf6ada37fe2
Bogosel Daniel <daniel.bogosel@nexttech.ro>
2020-11-09 16:02:51 +0200

    First exercise

---
+++ src/main/java/courses/exercice1/Main.java
@@ -0,0 +1,36 @@
+package courses.exercice1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int guessNumber = random.nextInt(10);
+        int playerNumber;
+        int numberOfTries = 0;
+        System.out.println("Guess the number between 0 and 10");
+
+        while (numberOfTries < 3) {
+            playerNumber = scanner.nextInt();
+            if (playerNumber > guessNumber) {
+                System.out.println("The number is too high");
+                numberOfTries++;
+
+            } else if (playerNumber < guessNumber) {
+                System.out.println("The number is too low");
+                numberOfTries++;
+
+            } else {
+                System.out.println("You guess the number");
+                break;
+            }
+        }
+
+        if (numberOfTries == 3) {
+            System.out.println("You lost");
+        }
+
+    }
+}
