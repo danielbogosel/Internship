57fc1166cf502bf835ab35a34a61db35a107c26a
Bogosel Daniel <daniel.bogosel@nexttech.ro>
2020-11-16 12:03:58 +0200

    Adding missing problems

--- src/main/java/week1/monday/exercise4/MainMentalist.java
+++ src/main/java/week1/monday/exercise4/MainMentalist.java
@@ -1,14 +1,31 @@
 package week1.monday.exercise4;
 
+
 import java.util.Scanner;
 
 public class MainMentalist {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        MentalistService mentalistService = new MentalistService();
-        mentalistService.generateString();
-
-
+        String result;
+        String playerNumber;
+        StringGenerator stringGenerator = new StringGenerator();
+        String generatedNumber = stringGenerator.generateString();
+        System.out.println("Guess the number");
+        System.out.println(generatedNumber);
+        do {
+            playerNumber = scanner.next();
+            result = "";
+            for (int i = 0; i < generatedNumber.length(); i++) {
+                if (generatedNumber.charAt(i) == playerNumber.charAt(i)) {
+                    result = result + "*";
+                } else if (generatedNumber.contains(String.valueOf(playerNumber.charAt(i)))) {
+                    result = result + "+";
+                } else {
+                    result = result + " ";
+                }
+            }
+            System.out.println(result);
+        } while (!result.equals("*****"));
+        System.out.println("You guessed the number, it was" + generatedNumber);
     }
 }
